ae317b81a47f52037915d68f363fb0998ce52ee4
Akashgadhiya <akashpatel1101@gmail.com>
2020-01-04 00:50:23 +0530

    Complete home page english version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'webpage';
+
+  ngOnInit() {
+    $(document).ready(function () {
+      $(".carousel").slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false
+      });
+    });
+  }
 }
+
