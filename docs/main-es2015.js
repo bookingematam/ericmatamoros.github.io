f7f0f9328af879ce15490af63f6636caa3349654
Akashgadhiya <akashpatel1101@gmail.com>
2020-01-07 02:57:25 +0530

    Added changes

--- docs/main-es2015.js
+++ docs/main-es2015.js
@@ -392,6 +392,7 @@ AppRoutingModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
     Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
         imports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"].forRoot(routes, {
                 anchorScrolling: 'enabled',
+                scrollPositionRestoration: 'enabled'
             })],
         exports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"]]
     })
--- docs/main-es5.js
+++ docs/main-es5.js
@@ -847,7 +847,8 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
     AppRoutingModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
       imports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"].forRoot(routes, {
-        anchorScrolling: 'enabled'
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'enabled'
       })],
       exports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"]]
     })], AppRoutingModule);
