58783fb11f3d49ea9c0d5fc88f69954b5f389860
Akashgadhiya <akashpatel1101@gmail.com>
2020-01-07 21:37:37 +0530

    Added build version

--- docs/es/vendor-es5.js
+++ docs/es/vendor-es5.js
@@ -86860,6 +86860,235 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
   },
 
   /***/
+  "./node_modules/emailjs-com/source/index.js":
+  /*!**************************************************!*\
+    !*** ./node_modules/emailjs-com/source/index.js ***!
+    \**************************************************/
+
+  /*! no static exports found */
+
+  /***/
+  function node_modulesEmailjsComSourceIndexJs(module, exports, __webpack_require__) {
+    "use strict";
+
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+
+    var EmailJSResponseStatus_1 = __webpack_require__(
+    /*! ./models/EmailJSResponseStatus */
+    "./node_modules/emailjs-com/source/models/EmailJSResponseStatus.js");
+
+    exports.EmailJSResponseStatus = EmailJSResponseStatus_1.EmailJSResponseStatus;
+
+    var UI_1 = __webpack_require__(
+    /*! ./services/ui/UI */
+    "./node_modules/emailjs-com/source/services/ui/UI.js");
+
+    var _userID = null;
+    var _origin = 'https://api.emailjs.com';
+
+    function sendPost(url, data, headers) {
+      if (headers === void 0) {
+        headers = {};
+      }
+
+      return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener('load', function (event) {
+          var responseStatus = new EmailJSResponseStatus_1.EmailJSResponseStatus(event.target);
+
+          if (responseStatus.status === 200 || responseStatus.text === 'OK') {
+            resolve(responseStatus);
+          } else {
+            reject(responseStatus);
+          }
+        });
+        xhr.addEventListener('error', function (event) {
+          reject(new EmailJSResponseStatus_1.EmailJSResponseStatus(event.target));
+        });
+        xhr.open('POST', url, true);
+
+        for (var key in headers) {
+          xhr.setRequestHeader(key, headers[key]);
+        }
+
+        xhr.send(data);
+      });
+    }
+
+    function appendGoogleCaptcha(templatePrams) {
+      var element = document.getElementById('g-recaptcha-response');
+
+      if (element && element.value) {
+        templatePrams['g-recaptcha-response'] = element.value;
+      }
+
+      element = null;
+      return templatePrams;
+    }
+    /**
+     * Initiation
+     * @param {string} userID - set the EmailJS user ID
+     * @param {string} origin - set the EmailJS origin
+     */
+
+
+    function init(userID, origin) {
+      _userID = userID;
+      _origin = origin || 'https://api.emailjs.com';
+    }
+
+    exports.init = init;
+    /**
+     * Send a template to the specific EmailJS service
+     * @param {string} serviceID - the EmailJS service ID
+     * @param {string} templateID - the EmailJS template ID
+     * @param {Object} templatePrams - the template params, what will be set to the EmailJS template
+     * @param {string} userID - the EmailJS user ID
+     * @returns {Promise<EmailJSResponseStatus>}
+     */
+
+    function send(serviceID, templateID, templatePrams, userID) {
+      var params = {
+        lib_version: '2.4.1',
+        user_id: userID || _userID,
+        service_id: serviceID,
+        template_id: templateID,
+        template_params: appendGoogleCaptcha(templatePrams)
+      };
+      return sendPost(_origin + '/api/v1.0/email/send', JSON.stringify(params), {
+        'Content-type': 'application/json'
+      });
+    }
+
+    exports.send = send;
+    /**
+     * Send a form the specific EmailJS service
+     * @param {string} serviceID - the EmailJS service ID
+     * @param {string} templateID - the EmailJS template ID
+     * @param {string | HTMLFormElement} form - the form element or selector
+     * @param {string} userID - the EmailJS user ID
+     * @returns {Promise<EmailJSResponseStatus>}
+     */
+
+    function sendForm(serviceID, templateID, form, userID) {
+      if (typeof form === 'string') {
+        form = document.querySelector(form);
+      }
+
+      if (!form || form.nodeName !== 'FORM') {
+        throw 'Expected the HTML form element or the style selector of form';
+      }
+
+      UI_1.UI.progressState(form);
+      var formData = new FormData(form);
+      formData.append('lib_version', '2.4.1');
+      formData.append('service_id', serviceID);
+      formData.append('template_id', templateID);
+      formData.append('user_id', userID || _userID);
+      return sendPost(_origin + '/api/v1.0/email/send-form', formData).then(function (response) {
+        UI_1.UI.successState(form);
+        return response;
+      }, function (error) {
+        UI_1.UI.errorState(form);
+        return Promise.reject(error);
+      });
+    }
+
+    exports.sendForm = sendForm;
+    exports.default = {
+      init: init,
+      send: send,
+      sendForm: sendForm
+    };
+    /***/
+  },
+
+  /***/
+  "./node_modules/emailjs-com/source/models/EmailJSResponseStatus.js":
+  /*!*************************************************************************!*\
+    !*** ./node_modules/emailjs-com/source/models/EmailJSResponseStatus.js ***!
+    \*************************************************************************/
+
+  /*! no static exports found */
+
+  /***/
+  function node_modulesEmailjsComSourceModelsEmailJSResponseStatusJs(module, exports, __webpack_require__) {
+    "use strict";
+
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+
+    var EmailJSResponseStatus =
+    /** @class */
+    function () {
+      function EmailJSResponseStatus(httpResponse) {
+        this.status = httpResponse.status;
+        this.text = httpResponse.responseText;
+      }
+
+      return EmailJSResponseStatus;
+    }();
+
+    exports.EmailJSResponseStatus = EmailJSResponseStatus;
+    /***/
+  },
+
+  /***/
+  "./node_modules/emailjs-com/source/services/ui/UI.js":
+  /*!***********************************************************!*\
+    !*** ./node_modules/emailjs-com/source/services/ui/UI.js ***!
+    \***********************************************************/
+
+  /*! no static exports found */
+
+  /***/
+  function node_modulesEmailjsComSourceServicesUiUIJs(module, exports, __webpack_require__) {
+    "use strict";
+
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+
+    var UI =
+    /** @class */
+    function () {
+      function UI() {}
+
+      UI.clearAll = function (form) {
+        form.classList.remove(this.PROGRESS);
+        form.classList.remove(this.DONE);
+        form.classList.remove(this.ERROR);
+      };
+
+      UI.progressState = function (form) {
+        this.clearAll(form);
+        form.classList.add(this.PROGRESS);
+      };
+
+      UI.successState = function (form) {
+        form.classList.remove(this.PROGRESS);
+        form.classList.add(this.DONE);
+      };
+
+      UI.errorState = function (form) {
+        form.classList.remove(this.PROGRESS);
+        form.classList.add(this.ERROR);
+      };
+
+      UI.PROGRESS = 'emailjs-sending';
+      UI.DONE = 'emailjs-success';
+      UI.ERROR = 'emailjs-error';
+      return UI;
+    }();
+
+    exports.UI = UI;
+    /***/
+  },
+
+  /***/
   "./node_modules/rxjs/_esm2015/index.js":
   /*!*********************************************!*\
     !*** ./node_modules/rxjs/_esm2015/index.js ***!
